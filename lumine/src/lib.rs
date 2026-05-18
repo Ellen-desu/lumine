b5ec592dc41b43fa5836a81bc4473650f19fa87a
airi <211204004+Ellen-desu@users.noreply.github.com>
2026-05-18 15:20:21 +0700

    refactor(lib): simplify the re-exports

--- lumine/src/lib.rs
+++ lumine/src/lib.rs
@@ -9,21 +9,16 @@ pub mod routing;
 pub mod types;
 
 #[doc(inline)]
-pub use crate::application::Lumine;
-
-#[doc(inline)]
-pub use crate::error::Error;
-
-#[doc(inline)]
-pub use crate::routing::{IntoResponse, Params, Path, Query, Route};
-
-#[doc(inline)]
-pub use crate::middleware::{Middleware, Next};
-
-#[doc(inline)]
-pub use crate::types::{Body, Request, Response, Result};
+pub use crate::{
+    application::Lumine,
+    error::Error,
+    middleware::{Middleware, Next},
+    routing::{IntoResponse, Params, Path, Query, Route},
+    types::{Body, Request, Response, Result},
+};
 
 #[cfg(feature = "bench")]
+#[doc(hidden)]
 pub use crate::internal::parser::{
     parse_body_for_bench, parse_headers_for_bench, parse_request_line_for_bench,
 };
