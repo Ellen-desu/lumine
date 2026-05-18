7fee09f1006ecd46153793f43cd1cb1e2786dc3f
airi <211204004+Ellen-desu@users.noreply.github.com>
2026-05-18 09:42:15 +0700

    refactor(lib): export the bench functions

--- lumine/src/lib.rs
+++ lumine/src/lib.rs
@@ -23,4 +23,9 @@ pub use crate::middleware::{Middleware, Next};
 #[doc(inline)]
 pub use crate::types::{Body, Request, Response, Result};
 
+#[cfg(feature = "bench")]
+pub use crate::internal::parser::{
+    parse_body_for_bench, parse_headers_for_bench, parse_request_line_for_bench,
+};
+
 pub use http;
