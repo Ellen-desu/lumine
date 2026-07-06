eaddf149b10d3d28c34decaddca5444dbac51a6f
airi <211204004+Ellen-desu@users.noreply.github.com>
2026-07-06 09:07:06 +0700

    Docs: Remove doc(hidden) from routing module

--- lumine/src/routing/mod.rs
+++ lumine/src/routing/mod.rs
@@ -4,8 +4,6 @@
 //! Routing is responsible for matching incoming request URIs against
 //! registered routes, and dispatching the request to the appropriate handler.
 
-#![doc(hidden)]
-
 pub mod route;
 pub mod route_entry;
 pub mod segment;
